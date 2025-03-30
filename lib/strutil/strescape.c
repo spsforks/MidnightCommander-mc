d0877a55eb90476085b514508fa35d5024e5d2a9
Andrew Borodin <aborodin@vmail.ru>
2025-03-30 13:51:33 +0300

    Use g_strdup() and g_free() instead of strdup() and free() respectively.
    Signed-off-by: Andrew Borodin <aborodin@vmail.ru>
    Signed-off-by: Yury V. Zaytsev <yury@shurup.com>

--- lib/strutil/strescape.c
+++ lib/strutil/strescape.c
@@ -62,7 +62,7 @@ str_escape (const char *src, gsize src_len, const char *escaped_chars,
         return NULL;
 
     if (*src == '\0')
-        return strdup ("");
+        return g_strdup ("");
 
     ret = g_string_new ("");
 
@@ -113,7 +113,7 @@ str_unescape (const char *src, gsize src_len, const char *unescaped_chars,
         return NULL;
 
     if (*src == '\0')
-        return strdup ("");
+        return g_strdup ("");
 
     ret = g_string_sized_new (16);
 
